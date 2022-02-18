68a10642b1d03a38c6c180677f6e1c412081f666
baraian <16649839+RRasda@users.noreply.github.com>
2022-02-18 02:26:52 +0200

    cors

--- headers.php
+++ headers.php
@@ -1,6 +1,7 @@
 <?php
 $website = "https://scandiweb-self.vercel.app";
 header('Access-Control-Allow-Origin:' . $website);
+header("Access-Control-Allow-Headers:" . $website);
 header('Access-Control-Allow-Methods: GET, POST,DELETE,OPTIONS');
 header('Access-Control-Max-Age: 1000');
 header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
