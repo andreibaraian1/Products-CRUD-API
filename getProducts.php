a1b4eca30e826e8438b08cb1a8c452c33776b98c
baraian <16649839+RRasda@users.noreply.github.com>
2022-02-23 17:57:28 +0200

    typo

--- getProducts.php
+++ getProducts.php
@@ -1,5 +1,5 @@
 <?php
-require_once dirname(__FILE__) . '/Product/product.php';
+require_once dirname(__FILE__) . '/Product/Product.php';
 require_once dirname(__FILE__) . '/headers.php';
 
 $Product = new Product();
